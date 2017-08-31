b5f9399ad1aeaa310b562fccac1de25acea67913
piya_l <piya.tae@gmail.com>
2017-08-31 22:17:15 +0700

    Add ZUUL route path

--- client/src/main/java/it/me/tae/client/ClientApplication.java
+++ client/src/main/java/it/me/tae/client/ClientApplication.java
@@ -1,26 +1,19 @@
 package it.me.tae.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-import java.util.Map;
 
 @EnableOAuth2Sso
+@EnableZuulProxy
 @RestController
 @SpringBootApplication
 public class ClientApplication extends WebSecurityConfigurerAdapter {
@@ -35,30 +28,6 @@ public class ClientApplication extends WebSecurityConfigurerAdapter {
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
-    @Bean
-    public OAuth2RestTemplate restTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails resourceDetails) {
-        return new OAuth2RestTemplate(resourceDetails, oauth2ClientContext);
-    }
-
-
-
-
-    @Autowired
-    private OAuth2RestTemplate restTemplate;
-
-    @Value("${oauth2.resource.url}")
-    private String resourceUrl;
-
-
-    @RequestMapping("/admin")
-    public Map<String, String> user() {
-        return restTemplate.exchange(
-                resourceUrl + "/me",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<Map<String, String>>() {
-                }).getBody();
-    }
 
     @RequestMapping("/user")
     public Principal user(Principal principal) {
