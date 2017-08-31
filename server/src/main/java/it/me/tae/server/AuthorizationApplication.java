6e78e4fb5edb67a8519dbcab2140666e3adfe445
piya_l <piya.tae@gmail.com>
2017-08-31 17:42:45 +0700

    Config form login

--- server/src/main/java/it/me/tae/server/AuthorizationApplication.java
+++ server/src/main/java/it/me/tae/server/AuthorizationApplication.java
@@ -3,7 +3,10 @@ package it.me.tae.server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 
@@ -19,7 +22,21 @@ public class AuthorizationApplication {
                 .withUser("tae").password("secret").authorities("ROLE_USER", "ROLE_ADMIN");
     }
 
-
+    @Configuration
+    public static class LoginConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .formLogin()
+                        .and()
+                    .httpBasic()
+                        .and()
+                    .authorizeRequests()
+                        .anyRequest().authenticated();
+            // @formatter:on
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationApplication.class, args);
