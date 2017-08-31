4e712f309b0eca6e509860a91120f4142b086d65
piya_l <piya.tae@gmail.com>
2017-08-31 09:32:42 +0700

    Add more security check

--- resource/src/main/java/it/me/tae/sso/ResourceApplication.java
+++ resource/src/main/java/it/me/tae/sso/ResourceApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +19,17 @@ import java.util.Map;
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RestController
-public class ResourceApplication {
+public class ResourceApplication extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .authorizeRequests()
+                    .anyRequest().access("#oauth2.hasScope('openid')");
+    }
+
     @RequestMapping("/me")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public Map<String, String> me(Principal principal) {
